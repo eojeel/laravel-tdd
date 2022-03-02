525b4e1888cf5340607328b4910c455dd022596e
Joe Lee <joseph.lee@pcspecialist.co.uk>
2022-03-02 20:56:14 +0000

    #003 Testing Request Validation

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 It's time to take the techniques we learned in Laravel From Scratch, and put them to good use building your first real-world application. Together, we'll leverage TDD to create Birdboard: a minimal Basecamp-like project management app.
 
 This series will give us a wide range of opportunities to pull up our sleeves and test our Laravel chops. As always, we start from scratch
+
+ ## Run tests
+ ./vendor/bin/sail artisan test
--- tests/Unit/ProjectsTest.php
+++ tests/Unit/ProjectsTest.php
@@ -5,22 +5,37 @@ namespace Tests\Unit;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Project;
 
 class ProjectsTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
 
-   public function test_a_user_can_create_a_project()
-   {
-       $this->withoutExceptionHandling();
+    public function test_a_user_can_create_a_project()
+    {
+        $this->withoutExceptionHandling();
 
-       $attributes = [
-              'title' => $this->faker->sentence(),
-              'description' => $this->faker->paragraph()
-       ];
+        $attributes = [
+            'title' => $this->faker->sentence(),
+            'description' => $this->faker->paragraph()
+        ];
 
-       $this->post('/projects', $attributes);
+        $this->post('/projects', $attributes);
 
-       $this->assertDatabaseHas('projects', $attributes);
-   }
+        $this->assertDatabaseHas('projects', $attributes);
+    }
+
+    public function test_a_project_requires_a_title()
+    {
+        $attributes = Project::factory()->raw(['title' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
+    }
+
+    public function test_a_project_requires_a_description()
+    {
+        $attributes = Project::factory()->raw(['description' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
+    }
 }
