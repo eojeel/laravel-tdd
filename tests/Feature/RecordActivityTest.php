8f0a7168a185cc3695b03349e1705a2a508215bb
Joe Lee <eojeel@users.noreply.github.com>
2022-09-08 20:38:05 +0100

    dev #025 poly activity notes

--- tests/Feature/RecordActivityTest.php
+++ tests/Feature/RecordActivityTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Models\Task;
 use Facades\Tests\Setup\ProjectFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -12,9 +13,10 @@ class RecordActivityTest extends TestCase
 
     function test_creating_a_project()
     {
+
         $project = ProjectFactory::create();
 
-        $this->assertCount(1 ,$project->activity);
+        $this->assertCount(1, $project->activity);
         $this->assertEquals('created', $project->activity[0]->description);
     }
 
@@ -26,7 +28,6 @@ class RecordActivityTest extends TestCase
 
         $this->assertCount(2, $project->activity);
         $this->assertEquals('updated', $project->activity->last()->description);
-
     }
 
     function test_creating_a_new_task()
@@ -36,7 +37,12 @@ class RecordActivityTest extends TestCase
         $project->addTask('Some Task');
 
         $this->assertCount(2, $project->activity);
-        $this->assertEquals('created_task', $project->activity->last()->description);
+
+        tap($project->activity->last(), function ($activity) {
+            $this->assertEquals('created_task', $activity->description);
+            $this->assertInstanceOf(Task::class, $activity->subject);
+            $this->assertEquals('Some Task', $activity->subject->body);
+        });
     }
 
     function test_completing_a_new_task()
@@ -50,7 +56,11 @@ class RecordActivityTest extends TestCase
             ]);
 
         $this->assertCount(3, $project->activity);
-        $this->assertEquals('completed_task', $project->activity->last()->description);
+
+        tap($project->activity->last(), function ($activity) {
+            $this->assertEquals('completed_task', $activity->description);
+            $this->assertInstanceOf(Task::class, $activity->subject);
+        });
     }
 
     function test_incompleting_a_task()
@@ -66,12 +76,12 @@ class RecordActivityTest extends TestCase
         $this->assertCount(3, $project->activity);
 
         $this->actingAs($project->owner)
-        ->patch($project->tasks[0]->path(), [
-            'body' => 'foobar',
-            'completed' => false
-        ]);
+            ->patch($project->tasks[0]->path(), [
+                'body' => 'foobar',
+                'completed' => false
+            ]);
 
-        $project = $project->refresh ();
+        $project = $project->refresh();
 
         $this->assertCount(4, $project->activity);
 
@@ -85,6 +95,5 @@ class RecordActivityTest extends TestCase
         $project->tasks[0]->delete();
 
         $this->assertCount(3, $project->activity);
-
     }
 }
